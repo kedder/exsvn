298008d70a63d6131fe1fd5b63f60d492a1b2b21
Andrey Lebedev <andrey.lebedev@gmail.com>
2009-03-04 16:09:25 +0200

    nice formatting of status output

--- exsvn/status.py
+++ exsvn/status.py
@@ -2,6 +2,23 @@ import pysvn
 
 from error import ApplicationError
 
+wc_status_kind_map = {
+	pysvn.wc_status_kind.added: 'A',
+	pysvn.wc_status_kind.conflicted: 'C',
+	pysvn.wc_status_kind.deleted: 'D',
+	pysvn.wc_status_kind.external: 'X',
+	pysvn.wc_status_kind.ignored: 'I',
+	pysvn.wc_status_kind.incomplete: '!',
+	pysvn.wc_status_kind.missing: '!',
+	pysvn.wc_status_kind.merged: 'G',
+	pysvn.wc_status_kind.modified: 'M',
+	pysvn.wc_status_kind.none: ' ',
+	pysvn.wc_status_kind.normal: ' ',
+	pysvn.wc_status_kind.obstructed: '~',
+	pysvn.wc_status_kind.replaced: 'R',
+	pysvn.wc_status_kind.unversioned: '?',
+}
+
 def get_status(ctx, dir):
 	try:
 		full_status = ctx.client.status(dir)
@@ -9,10 +26,10 @@ def get_status(ctx, dir):
 		raise ApplicationError(e)
 
 	ignore = [pysvn.wc_status_kind.none, pysvn.wc_status_kind.normal]
-	status = (s for s in status if s.text_status not in ignore)
+	status = (s for s in full_status if s.text_status not in ignore)
 	return status
 	
 
 def print_status(status):
 	for item in status:
-		print item
+		print "  ",wc_status_kind_map[item.text_status], item.path
